d1b8242dda43f0869f42903e299958089c789ce3
Andrew20012019 <46867131+Andrew20012019@users.noreply.github.com>
2023-04-25 13:23:10 +0900

    Add files via upload

---
+++ BNC TERMINAL.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+driver = webdriver.Chrome()
+
+
+# BNC TERMINAL
+# Send a GET request to the website you want to scrape
+url = "https://info.bct2-4.com/infoservice/index.html"
+driver=webdriver.Chrome()
+
+driver.get(url)
+#element = driver.find_element(By.ID, "mainframe_vframeset_hframeset_bodyframe_introframe_popNotice_titlebar_closebuttonAlignImageElement")
+#mainframe_vframeset_hframeset_leftframe_form_grd_menu_body_gridrow_1_cell_1_0_controltreeTextBoxElement > div
+#element.click()
+
+driver.quit()
+
